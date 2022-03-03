5d9ac99f183fbcf2fcbfa452e0759817a4378656
aman <amanthapliyal14>
2022-03-03 10:27:25 +0530

    ui improvement in homescreen

--- lib/screens/Home_Screen.dart
+++ lib/screens/Home_Screen.dart
@@ -36,126 +36,113 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              leading: IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {},
+      child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.orange, Colors.white])),
+
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(28.0),
+                child: Center(child: Text("Please Choose A category",style: TextStyle(color: Colors.white,fontSize: 26,fontWeight: FontWeight.bold),)),
               ),
-              pinned: _pinned,
-              expandedHeight: 160.0,
-              flexibleSpace: const FlexibleSpaceBar(
-                title: Text(
-                  "Want Some Consusltancy",
-                  style: TextStyle(
-                      fontSize: 19,
-                      fontWeight: FontWeight.w900,
-                      fontFamily: 'OpenSans-Regular'),
-                ),
-                background: BackGroundImage(),
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: SizedBox(
-                height: 40,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
-                  itemCount: chipsCustom.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return chipsCustom[index];
-                  },
-                ),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Card(
-                      color: Colors.orange.shade400,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20),
-                        ),
-                      ),
-                      elevation: 3,
-                      shadowColor: Color(0XFFDCB5A8),
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Container(
-                          width: MediaQuery.of(context).size.width,
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                GestureDetector(
-                                  onTap: () {
-                                    Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                            builder: (context) => DetailsScreen(
-                                                serviceName: _services[index]
-                                                    ["service_name"])));
-                                  },
-                                  child: ListTile(
-                                    title: Center(
-                                        child: Text(
-                                      _services[index]["service_name"],
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w900,
-                                          fontFamily: 'OpenSans-Regular'),
-                                    )),
-                                    subtitle: Center(
-                                        child: Text(
-                                      _services[index]["subtitle"],
-                                      style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w900,
-                                          fontFamily: 'OpenSans-Regular'),
-                                    )),
-                                    trailing: CircleAvatar(
-                                      backgroundColor: Colors.white,
-                                      maxRadius: 30,
-                                      child: Icon(
-                                        IconData(
-                                            int.parse(_services[index]["icon"]),
-                                            fontFamily: 'MaterialIcons'),
-                                        color: Colors.red[900],
-                                        size: 40,
-                                      ),
-                                    ),
-                                    leading: CircleAvatar(
-                                        maxRadius: 25,
-                                        backgroundColor: Colors.white,
-                                        child: Padding(
-                                          padding: EdgeInsets.all(2),
-                                          child: Container(
-                                            decoration: BoxDecoration(
-                                              image: DecorationImage(
-                                                image: NetworkImage(
-                                                  _services[index]
-                                                      ["custom_icon"],
-                                                ),
-                                              ),
+
+              Expanded(
+                child: Container(
+
+                  child: ListView.builder(
+                      itemCount: _services.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Card(
+                            color: index%2==0?Colors.orange.shade400:Colors.yellow,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20),
+                              ),
+                            ),
+                            elevation: 3,
+                            shadowColor: Color(0XFFDCB5A8),
+                            child: Padding(
+                              padding: EdgeInsets.all(18.0),
+                              child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () {
+                                          Navigator.of(context).push(
+                                              MaterialPageRoute(
+                                                  builder: (context) => DetailsScreen(
+                                                      serviceName: _services[index]
+                                                      ["service_name"])));
+                                        },
+                                        child: ListTile(
+                                          title: Center(
+                                              child: Text(
+                                                _services[index]["service_name"],
+                                                style: TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.w900,
+                                                    fontFamily: 'OpenSans-Regular'),
+                                              )),
+                                          subtitle: Center(
+                                              child: Text(
+                                                _services[index]["subtitle"],
+                                                style: TextStyle(
+                                                    fontSize: 18,
+                                                    fontWeight: FontWeight.w900,
+                                                    fontFamily: 'OpenSans-Regular'),
+                                              )),
+                                          trailing: CircleAvatar(
+                                            backgroundColor: Colors.white,
+                                            maxRadius: 30,
+                                            child: Icon(
+                                              IconData(
+                                                  int.parse(_services[index]["icon"]),
+                                                  fontFamily: 'MaterialIcons'),
+                                              color: Colors.red[900],
+                                              size: 40,
                                             ),
                                           ),
-                                        )),
-                                  ),
-                                ),
-                              ]),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-                childCount: _services.length,
-              ),
-            ),
-          ],
+                                          leading: CircleAvatar(
+                                              maxRadius: 25,
+                                              backgroundColor: Colors.white,
+                                              child: Padding(
+                                                padding: EdgeInsets.all(2),
+                                                child: Container(
+                                                  decoration: BoxDecoration(
+                                                    image: DecorationImage(
+                                                      image: NetworkImage(
+                                                        _services[index]
+                                                        ["custom_icon"],
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                              )),
+                                        ),
+                                      ),
+                                    ]),
+                              ),
+                            ),
+                          ),
+                        );
+                      },),
+                ),
+              )
+            ],
+          )
         ),
       ),
     );
